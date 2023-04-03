ec67957bc33c313f2eff76d84a7d84dfc291c470
Amira Lee <amiralee97@gmail.com>
2023-04-03 16:03:47 -0400

    Display feedback for correct/incorrect answers

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
-let questionCounter = 0; 
+let questionCounter = 0;
 let availableQuestions = [];
 
 let questions = [
@@ -15,7 +15,7 @@ let questions = [
         choice3: "<js>",
         choice4: "<scripting>",
         answer: 1
-    }, 
+    },
     {
         question: "What is the correct syntax for referring to an external script called 'xxx.js' ?",
         choice1: "<script href= 'xxx.js>",
@@ -42,40 +42,47 @@ startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions)
+
     getNewQuestion();
 };
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter  >= MAX_QUESTIONS) {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
         //go to end page
-        return window.location.assign('/end.html');  
+        return window.location.assign('/end.html');
     }
     questionCounter++;
-   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-   currentQuestion = availableQuestions[questionIndex];
-   question.innerText = currentQuestion.question;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
 
-   choices.forEach( choice => {
-    const number = choice.dataset['number'];
-    choice.innerText = currentQuestion['choice' + number];
-   });
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
 
-   availableQuestions.splice(questionIndex, 1);
+    availableQuestions.splice(questionIndex, 1);
 
-   acceptingAnswers = true;
+    acceptingAnswers = true;
 };
 
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return;
+        if (!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
 
-        console.log(selectedAnswer);
-        getNewQuestion();
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000)
     });
 });
 startGame();
