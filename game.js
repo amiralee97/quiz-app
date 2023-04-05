943d71d7090d4bc5405548c3fdb3cc0ba28aa941
Amira Lee <amiralee97@gmail.com>
2023-04-05 16:44:51 -0400

    Add HUD display

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
+
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -52,6 +55,9 @@ getNewQuestion = () => {
         return window.location.assign('/end.html');
     }
     questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -76,6 +82,9 @@ choices.forEach(choice => {
 
         const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
+        if (classToApply === 'correct'){
+            incrementScore(CORRECT_BONUS);
+        }
 
         selectedChoice.parentElement.classList.add(classToApply);
 
@@ -85,4 +94,9 @@ choices.forEach(choice => {
         }, 1000)
     });
 });
+
+incrementScore = num => {
+    score +=num;
+    scoreText.innerText = score;
+}
 startGame();
